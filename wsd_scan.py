bca8f8c6372ca67a84cbe9b834762105826caff0
Patrick Roncagliolo <ronca.pat@gmail.com>
2018-02-08 10:26:07 +0100

    Initial ws_scan getscannerelements support

--- wsd_scan.py
+++ wsd_scan.py
@@ -11,12 +11,12 @@ import argparse
 import requests
 import lxml.etree as etree
 
-def WSD_GetPrinterElements(hosted_print_service):
-    data = messageFromFile("ws-print_getprinterelements.xml", FROM=urn, TO=hosted_print_service.ep_ref_addr)
+def WSD_GetScannerElements(hosted_print_service):
+    data = messageFromFile("ws-scan_getscannerelements.xml", FROM=urn, TO=hosted_print_service.ep_ref_addr)
     r = requests.post(hosted_print_service.ep_ref_addr, headers=headers, data=data)
 
     x = etree.fromstring(r.text)
-    if debug: print ('##\n## GET PRINTER ELEMENTS RESPONSE\n##\n')
+    if debug: print ('##\n## GET SCANNER ELEMENTS RESPONSE\n##\n')
     if debug: print (etree.tostring(x, pretty_print=True, xml_declaration=True).decode('ascii'))
 
 
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         print(a)
         (ti, hss) = wsd_transfer.WSD_Get(a)
         for b in hss:
-            if "wprt:PrintServiceType" in b.types:
+            if "wscn:ScannerServiceType" in b.types:
                 print(b)
                 debug = True
-                WSD_GetPrinterElements(b)
+                WSD_GetScannerElements(b)
