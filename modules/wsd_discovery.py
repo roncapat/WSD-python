84f32cddada86dc992048db3e259262a93183c49
Patrick Roncagliolo <ronca.pat@gmail.com>
2018-02-15 15:07:50 +0100

    Move structures to a new file

--- modules/wsd_discovery.py
+++ modules/wsd_discovery.py
@@ -2,35 +2,13 @@
 #-*- encoding: utf-8 -*-
 
 from wsd_common import *
+from wsd_structures import *
 
 import argparse
 import struct
 import socket
 import lxml.etree as etree
 
-
-class TargetService:
-    def __init__(self):
-        self.ep_ref_addr = ""
-        self.types = []
-        self.scopes = []
-        self.xaddrs = []
-        self.meta_ver = 0
-    
-    def __str__(self):
-        s = ""
-        s += "EndPoint reference:   %s\n" % self.ep_ref_addr
-        s += "Metadata version:     %d\n" % self.meta_ver
-        s += "Implemented Types:    %s\n" % ', '.join(self.types)
-        s += "Assigned Scopes:      %s\n" % ', '.join(self.scopes)
-        s += "Transport addresses:  %s\n" % ', '.join(self.xaddrs)
-        return s
-        
-    def __eq__(self, other):
-        return self.ep_ref_addr == other.ep_ref_addr
-    
-    def __hash__(self):
-        return self.ep_ref_addr.__hash__()
 
 
 NSMAP = {"soap": "http://www.w3.org/2003/05/soap-envelope",
--- modules/wsd_transfer.py
+++ modules/wsd_transfer.py
@@ -3,63 +3,12 @@
 
 import wsd_discovery
 from wsd_common import *
+from wsd_structures import *
 
 import uuid
 import argparse
 import requests
 import lxml.etree as etree
-
-
-class TargetInfo:
-    def __init__(self):
-        self.manufacturer = ""
-        self.manufacturer_url = ""
-        self.model_name = ""
-        self.model_number = ""
-        self.model_url = ""
-        self.presentation_url = ""
-        self.device_cat = []
-        self.friendly_name = ""
-        self.fw_ver = ""
-        self.serial_num = ""
-
-    def __str__(self):
-        s = ""
-        s += "Friendly name:        %s\n" % self.friendly_name
-        s += "Model name:           %s\n" % self.model_name
-        s += "Model number:         %s\n" % self.model_number
-        s += "Device categories:    %s\n" % ', '.join(self.device_cat)
-        s += "Manufacturer:         %s\n" % self.manufacturer
- 
-        s += "Firmware version:     %s\n" % self.fw_ver
-        s += "Serial number:        %s\n" % self.serial_num
- 
-        s += "Manufacturer URL:     %s\n" % self.manufacturer_url
-        s += "Model URL:            %s\n" % self.model_url
-        s += "Web UI URL:           %s\n" % self.presentation_url
-        return s
-    
-
-class HostedService:
-    def __init__(self):
-        self.types = []
-        self.service_id = ""
-        self.hardware_id = ""
-        self.compatible_id = ""
-        self.service_address = ""
-        self.ep_ref_addr = ""
-        
-    def __str__(self):
-        s = ""
-        s += "Service path:         %s\n" % self.ep_ref_addr
-        s += "Service ID:           %s\n" % self.service_id
-        s += "Service types:        %s\n" % ', '.join(self.types)
-        s += "Compatible IDs:       %s\n" % self.compatible_id
-        s += "Hardware ID:          %s\n" % self.hardware_id
-        s += "Service address:      %s\n" % self.service_address
-        return s
-
-        
 
 NSMAP = {"soap": "http://www.w3.org/2003/05/soap-envelope",
 "wsa": "http://schemas.xmlsoap.org/ws/2004/08/addressing",
