7827e051265d2c03f0f5b39a5d04a57d15c7f9f1
Patrick Roncagliolo <ronca.pat@gmail.com>
2018-02-25 18:43:27 +0100

    Add setup.py
    Signed-off-by: Patrick Roncagliolo <ronca.pat@gmail.com>

---
+++ modules/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='WSD-python',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'modules'},
+    url='https://github.com/roncapat/WSD-python',
+    license='GPL v3.0',
+    author='Patrick Roncagliolo',
+    author_email='ronca.pat@gmail.com',
+    description='A library for Web Services for Devices support'
+)
